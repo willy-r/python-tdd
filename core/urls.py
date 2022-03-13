86993db6ca16bc5585be6a96e6e716113bcab821
William Rodrigues <williamrodrigues2442@gmail.com>
2022-03-13 14:28:25 -0300

    feat: first unit test and url mapping, dummy view

--- core/urls.py
+++ core/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+from todo import views
+
 urlpatterns = [
+    url(r'^$', views.home_page, name='home'),
     url(r'^admin/', admin.site.urls),
 ]
